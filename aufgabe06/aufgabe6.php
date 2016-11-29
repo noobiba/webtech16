145adf4c681c03738362b45f4fbd70fe156b4bdb
noobiba <noobanine@gmail.com>
2016-11-29 15:02:07 +0100

    Add normalize.css and bootstrap.css.

--- aufgabe06/aufgabe6.php
+++ aufgabe06/aufgabe6.php
@@ -2,6 +2,9 @@
 <html>
 <head>
 <meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" type="text/css" href="../css/normalize.css">
+<link href="../css/bootstrap.css" rel="stylesheet">
 <title>Datei einlesen</title>
 </head>
 <body>
