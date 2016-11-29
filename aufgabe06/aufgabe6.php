dc49c4e44bcfd6e99774b71263df7bba624ea710
noobiba <noobanine@gmail.com>
2016-11-29 14:31:48 +0100

    Test from /Sites folder

---
+++ aufgabe06/aufgabe6.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Datei einlesen</title>
+</head>
+<body>
+	<h1>Datei einlesen</h1>
+<?php
+/*
+ * falls das Zeilenende der einzulesenden Datei nicht korrekt 
+ * erkannt werden sollte, dann sollte die folgende Anweisung 
+ * ausgeführt werden
+ */
+ini_set("auto_detect_line_endings", true);
+/* Öffnen der Datei zum Lesen "r"
+ * Datei muss im gleichen Verzeichnis liegen wie aufgabe6.php
+ * andernfalls Pfadangabe ändern
+ * @ unterdrückt evtl Warnungen
+ */
+$file = @fopen ( "./mockdatatext", "r" );
+/*
+ * wenn das Öffnen der Datei funktioniert hat, ist $file TRUE
+ * wenn nicht, dann FALSE
+ */
+if (! $file) {
+	echo "Es ist ein Problem beim Öffnen der Datei 'mockupdatatext' aufgetreten.";
+} else {
+	/*
+	 * feof - end of file
+	 * prüft, ob ein Dateizeiger am Ende der Datei steht
+	 */
+	while ( ! feof ( $file ) ) {
+		/*
+		 * fegts() liest eine Zeile einer Datei aus
+		 * fgets() gibt einen String zürück
+		 * nach Aufruf von fgets() steht der Dateizeiger 
+		 * in der nächsten Zeile (außer, es wurde eine 
+		 * Leselänge als 2. Parameter fgets übergeben)
+		 */
+		$current_line = fgets ( $file );
+		echo $current_line . "<br/>";
+	}
+	fclose ( $file );
+}
+?>
+</body>
+</html>
