ebac05ec9354dbb273d90f9a998a94c880966a16
noobiba <noobanine@gmail.com>
2017-02-04 18:59:38 +0100

    Add other code pieces. Add Aufgabe 9

--- aufgabe09/index.php
+++ aufgabe09/index.php
@@ -13,16 +13,52 @@
     <h1>Mausereignisse behandeln mit JavaScript</h1>
     
     <div><a href="../index.html">Home</a></div>
+    <p></p>
 
-    <div id="demo" style="width:200px; height:200px; background-color:red;"
-    onclick="document.getElementById('demo').style.backgroundColor = 'green'"
-    ondblclick="document.getElementById('demo').style.lineHeight='200px'
-                document.getElementById('demo').innerHTML='Doppelklick'
-                document.getElementById('demo').style.textAlign='center'"></div>
+    <div id="demo" 
+      style="width:200px; height:200px; background-color:red;"
+      onclick="document.getElementById('demo').style.backgroundColor = 'green'"
+      ondblclick="document.getElementById('demo').style.lineHeight ='200px'
+                  document.getElementById('demo').innerHTML ='Doppelklick'
+                  document.getElementById('demo').style.textAlign ='center'"
+      onmouseenter="toggleDivSize(this)"      
+      onmouseleave="document.getElementById('demo').style.width ='200px'
+                    document.getElementById('demo').style.height ='200px'""></div>
 
 
+    <script>
+      function toggleDivSize(){
+        var klein = document.getElementById('demo').style.height ='200px';
 
+
+          if (klein) {
+            document.getElementById('demo').style.width ='300px';
+            document.getElementById('demo').style.height ='300px';
+          }else{
+            document.getElementById('demo').style.width ='200px';
+            document.getElementById('demo').style.height ='200px';
+          }
+      }
+    </script>
+
+    <p></p>
+    <hr/>
+    <p></p>
+
+    <h1>Tastaturereignisse behandeln mit JavaScript</h1>
+
+    <input type="text" id="textfeld" value="" name="" size="20" onkeyup="zeigeWert()" />
+    <p></p>
+    <div id="inhalt"></div>
+
+
+    <script>
+       function zeigeWert(){
+          var wert = document.getElementById('textfeld').value;
+          document.getElementById('inhalt').innerHTML = "Eingegeben wurde: " + wert;
+       }
+    </script>
    
-</div>
+  </div>
 </body>
 </html>
---
+++ js/quadratzahl.js
@@ -0,0 +1,7 @@
+//Javascript
+function berechneQuadratzahl(){
+	var zahl = document.quadratForm.eingabe.value;
+	var ergebnis = zahl * zahl;
+
+	alert ("Das Quadrat von " + zahl + " ist " + ergebnis);
+}
