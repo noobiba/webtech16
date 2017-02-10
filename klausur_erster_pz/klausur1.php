dddedea68b065736a89aae8473a088b29af7f662
noobiba <noobanine@gmail.com>
2017-02-10 18:09:12 +0100

    Add structure to klausur1.php

--- klausur_erster_pz/klausur1.php
+++ klausur_erster_pz/klausur1.php
@@ -4,12 +4,40 @@
 <title>WebTech</title>
 <meta charset="utf-8">
 <meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet"
-	href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
 
 </head>
 <body>
-Anzeigetest klausur1.php -- kann weg
+	
+		<header>WebTech Klausur</header>
+
+			<div class="oben">
+				Datei laden
+				Suchfeld
+			</div> <!-- class oben -->
+
+			<div class="main">
+				<div class="linkeseite">
+					Original
+					Textfeld des Originals
+					Kopieren
+					Text speichern
+					Suchbegriffe			
+				</div> <!-- class linkeseite -->
+
+				<div class="rechteseite">
+					Kopie
+					Textfeld der Kopie	
+				</div> <!-- class rechteseite -->
+			</div> <!-- class main -->
+
+			<div class="unten">
+					Datei laden
+					Suchfeld
+			</div> <!-- class oben -->
+
+
+		<footer>Tanja Cerri</footer>
 </body>
 
 </html>
